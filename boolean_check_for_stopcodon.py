e1c67329b62edbba8db901c9244c700936b3aff1
Fisha101 <149829145+Fisha101@users.noreply.github.com>
2025-10-15 00:02:30 +0400

    Add files via upload

---
+++ boolean_check_for_stopcodon.py
@@ -0,0 +1,38 @@
+#program that checks if a given DNA sequence contains an in-frame stop codon.
+
+dna = input("Enter DNA sequence: ")
+frame_input = input("Enter reading frame (number) [press Enter to use default 0]: ")
+if frame_input == "":
+    frame = 0
+else:
+    frame = int(frame_input)
+
+def has_stop_codon(dna, frame=0):
+    'This program that checks if a given DNA sequence contains an in-frame stop codon.'
+    dna.lower()
+    stop_codon_found = False
+    stop_codons = ['tga', 'tag', 'taa']
+    for i in range(frame, len(dna), 3):
+        codon = dna[i:i+3]
+        if codon in stop_codons:
+            stop_codon_found = True
+            break
+    
+    if True :
+        print('Input sequence has an in frame stop codon.')
+    else:
+        print('Input sequence does NOT have an in frame stop codon.')        
+        
+has_stop_codon(dna, frame)
+
+
+
+        
+
+
+    
+    
+
+
+    
+    
