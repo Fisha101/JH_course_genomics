581311028614457a104f7a6839ab120ac2dff487
Fisha101 <149829145+Fisha101@users.noreply.github.com>
2025-10-15 18:44:19 +0400

    Add files via upload

---
+++ fasta_dictionary.py
@@ -0,0 +1,22 @@
+#builds dictionary with all sequences form fasta file
+try:
+    f = open('my_seq.fasta')
+except IOError:
+    print ('file not found')
+    
+
+seqs_dict = {}
+for line in f:
+    line = line.rstrip()
+
+    if line[0]=='>':
+        words = line.split()
+        name = words[0][1:]
+        seqs_dict [name] = ''
+
+    else :
+        seqs_dict [name] = seqs_dict [name] + line
+        
+f.close()       
+
+print (seqs_dict)
